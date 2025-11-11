b1a3d7a7b228aa0d9b3378a553034059bc5c5152
jxlee007 <107770093+jxlee007@users.noreply.github.com>
2025-11-12 00:27:21 +0530

    Set site URL in astro.config.mjs
    O yeah

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
   integrations: [react(), tailwind()],
-  site: '',
+  site: 'https://jxlee007.github.io/SPY',
   // Serve static assets from src/public so references like /main.js and /images/* work
   publicDir: 'src/public',
   vite: {
